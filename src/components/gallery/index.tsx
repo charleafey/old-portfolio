ff2e7012266921548cb73f5582a7877d0850b511
chelffey <chaffeychelsea@gmail.com>
2024-03-30 12:02:59 +1100

    added more to gallery

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import KazuhaImage from '../../assets/images/genshin-kazuha-leaves.png';
 import RaidenImage from '../../assets/images/genshin-raiden-lotus.png';
 import ScaraImage from '../../assets/images/genshin-scara-cat.png';
+import VentiImage from '../../assets/images/genshin-venti-angel.png';
+import YaeImage from '../../assets/images/genshin-yae-miko-sakura.png';
 
 const galleryDisplayStyles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     gap: '20px',
-}   
+    flexFlow: 'row wrap',
+    maxWidth: '1200px',
+}
+
+const containerStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '20px',
+}
 
 export default function Gallery() {
     return (
+        <div style={containerStyles}>
         <div style={galleryDisplayStyles}>
             <img src={KazuhaImage} alt="Genshin Kazuha Leaves Artwork" height='500px' />
             <img src={RaidenImage} alt="Genshin Raiden Lotus Artwork" height='500px' />
             <img src={ScaraImage} alt="Genshin Scaramouche Artwork" height='500px' />
+            <img src={VentiImage} alt="Genshin Venti Artwork" height='500px' />
+            <img src={YaeImage} alt="Genshin Yae Miko Artwork" height='500px' />
+        </div>
         </div>
     )
 }
