f38690d6b28ecf1b787c2592a62b018f53566237
chelffey <chaffeychelsea@gmail.com>
2024-03-30 11:29:06 +1100

    begin styling pattern

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { ReactComponent as InstaSvg } from '../../assets/insta.svg';
 
+const titleStyles = {
+    fontSize: '64px',
+    fontWeight: 'normal'
+}
+
 export default function Home() {
     return (
         <div>
-            <h1>charraxeon</h1>
+            <h1 style={titleStyles}>charraxeon</h1>
             <a href="https://www.instagram.com/charraxeon/">
                 <InstaSvg/>
             </a>
