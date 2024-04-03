1c942e063a0b69f060e6174c9e578cb2833722f3
chelffey <chaffeychelsea@gmail.com>
2024-04-03 19:09:20 +1100

    refactor to header component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { ReactComponent as InstaSvg } from '../../assets/insta.svg';
 import Gallery from '../../components/gallery';
-import './styles.css';
-
-const titleStyles = {
-    fontSize: '64px',
-    fontWeight: 'normal',
-}
-
-const headerStyles = {
-    margin: '30px',
-    paddingBottom: '20px',
-    borderBottom: '2px solid lightgray',
-}
+import Header from '../../components/header';
 
 const Home : React.FunctionComponent = () => {
     return (
         <div>
-            <div style={headerStyles}>
-                <h1 style={titleStyles}>charraxeon</h1>
-                <p>charley (they/them)</p>
-                <p><em>asian australian artist and chaser of colours through the trees.</em></p>
-                <a href="https://www.instagram.com/charraxeon/">
-                    <InstaSvg className="instaIcon"/>
-                </a>
-            </div>
+            <Header/>
             <Gallery/>
         </div>
     );
