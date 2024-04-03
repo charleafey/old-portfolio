0c2215f5ec77ecbd9815c765ea1f96b93cbff807
chelffey <chaffeychelsea@gmail.com>
2024-04-03 19:31:29 +1100

    browser router setup for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Home from './pages/home';
 import './App.css';
+import Header from './components/header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Gallery from '../../components/gallery';
-import Header from '../../components/header';
 import { illustrationsImageList } from '../../assets/gallery-config/illustrations';
 
 const Home : React.FunctionComponent = () => {
     return (
         <React.Fragment>
-            <Header/>
             <Gallery imageList={illustrationsImageList}/>
         </React.Fragment>
     );
